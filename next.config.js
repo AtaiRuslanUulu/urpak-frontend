63d622f9363c5b80387d0a681bcb2210387876d4
AtaiRuslanUulu <95936930+AtaiRuslanUulu@users.noreply.github.com>
2025-05-10 00:53:34 +0200

    updated frontend for logos

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   images: {
-    domains: ["urpak.kg", "api.urpak.kg"],
+    domains: ["urpak.kg", "api.urpak.kg","urpak.s3.eu-central-1.amazonaws.com",],
     remotePatterns: [
       {
         protocol: 'https',
